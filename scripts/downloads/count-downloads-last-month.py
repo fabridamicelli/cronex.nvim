0ed9250b465d562461cbc80690dc2a4828b63498
Fabrizio Damicelli <fabridamicelli@gmail.com>
2025-04-06 11:51:55 +0200

    fix get-git-downloads workflow

--- scripts/downloads/count-downloads-last-month.py
+++ scripts/downloads/count-downloads-last-month.py
@@ -2,8 +2,14 @@ import json
 import pathlib
 
 counts = {}
-for o in pathlib.Path("downloads.jsonl").read_text().split():
-    entry = json.loads(o)
+lines = (
+    (pathlib.Path(".").resolve() / "scripts" / "downloads" / "downloads.jsonl")
+    .read_text()
+    .split()
+)
+assert lines, "No content found in downloads.jsonl"
+for line in lines:
+    entry = json.loads(line)
     counts[entry["date"]] = entry["count"]
 counts = dict(sorted(counts.items(), key=lambda i: i[1]))
 print(sum(list(counts.values())[-30:]))
--- scripts/downloads/get-git-downloads.sh
+++ scripts/downloads/get-git-downloads.sh
@@ -7,4 +7,4 @@ RESULT=$(
     gh api repos/$USERNAME/$REPOSITORY/traffic/clones |
         jq -r '.clones | group_by(.timestamp | split("T")[0]) | map({date: .[0].timestamp | split("T")[0], count: (map(.count) | add)}) | .[0:-1] | .[] | @json'
 )
-echo "${RESULT}" >>downloads.jsonl
+echo "${RESULT}" >>scripts/downloads/downloads.jsonl
