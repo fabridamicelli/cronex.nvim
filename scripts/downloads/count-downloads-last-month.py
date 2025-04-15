c6bfe1cbdbc6c27223b638bd56aa21ad8ac12f86
Fabrizio Damicelli <fabridamicelli@gmail.com>
2025-04-15 12:26:10 +0200

    fix count-downloads-last-month.py

--- scripts/downloads/count-downloads-last-month.py
+++ scripts/downloads/count-downloads-last-month.py
@@ -1,5 +1,6 @@
 import json
 import pathlib
+from collections import defaultdict
 
 N_LAST_DAYS = 30
 
@@ -10,9 +11,11 @@ lines = (
     .split()
 )
 assert lines, "No content found in downloads.jsonl"
+entries = defaultdict(list)
 for line in lines:
     entry = json.loads(line)
-    entries[entry["date"]] = entry["count"]
+    entries[entry["date"]].append(entry["count"])
 
-counts = sorted(entries.items(), key=lambda i: i[0])
+entries = sorted(entries.items(), key=lambda entry: entry[0])
+counts = [(date, max(vals)) for date, vals in entries]
 print(sum(count for _, count in counts[-N_LAST_DAYS:]))
