ffc5e7e91f334ef4e1ff474434e266418d119feb
Fabrizio Damicelli <fabridamicelli@gmail.com>
2025-04-28 13:30:07 +0200

    wip: handle error from system call

--- lua/cronex/explain.lua
+++ lua/cronex/explain.lua
@@ -3,47 +3,59 @@ M = {}
 
 M._cache = {}
 
---TODO:set only changed diagnostics
---TODO: set_explanations func
+
+local append_explanation = function(explanations, explanation, bufnr, lnum)
+	table.insert(explanations, {
+		bufnr = bufnr,
+		lnum = lnum,
+		col = 0,
+		message = explanation,
+		severity = vim.diagnostic.severity.HINT,
+	})
+end
+
+
+local schedule_explanations = function(explanations, ns, bufnr)
+	vim.schedule(function()
+		vim.diagnostic.set(ns, bufnr, explanations, {})
+	end)
+end
+
 
 M.explain = function(cmd, cron_expression, bufnr, lnum, ns, explanations)
 	local cached = M._cache[cron_expression]
 	if cached then
-		table.insert(explanations, {
-			bufnr = bufnr,
-			lnum = lnum,
-			col = 0,
-			message = cached,
-			severity = vim.diagnostic.severity.HINT,
-		})
-		vim.schedule(function()
-			vim.diagnostic.set(ns, bufnr, explanations, {})
-		end)
+		append_explanation(explanations, cached, bufnr, lnum)
+		schedule_explanations(explanations, ns, bufnr)
 	else
 		local on_exit = function(obj)
-			if obj.code ~= 0 then
-				print('ERROR: ', obj.stderr) --TODO: notify?, err?
-			end
 			local data = obj.stdout
-			if data ~= "" then ---TODO: check this works
+			if data ~= "" then ---TODO: add acceptance test?
 				-- Update cache
 				M._cache[cron_expression] = data
-				table.insert(explanations, {
-					bufnr = bufnr,
-					lnum = lnum,
-					col = 0,
-					message = data,
-					severity = vim.diagnostic.severity.HINT,
-				})
-				vim.schedule(function()
-					vim.diagnostic.set(ns, bufnr, explanations, {})
-				end)
+				append_explanation(explanations, data, bufnr, lnum)
+				schedule_explanations(explanations, ns, bufnr)
 			end
 		end
 
 		vim.system(
 			vim.iter({ cmd, cron_expression }):flatten():totable(),
-			{ text = true },
+			{
+				timeout = 2000, --TODO : add to config
+				text = true,
+				stderr = function(_, data)
+					if data then
+						vim.schedule(function()
+							local msg = string.format(
+								"Error calling cmd %s with args %s.\nStderr: \n %s", vim.inspect(cmd),
+								cron_expression,
+								data
+							)
+							vim.notify(msg)
+						end)
+					end
+				end
+			},
 			on_exit
 		)
 	end
